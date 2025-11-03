a0741c290842792f49e05f6236e1e251bb319922
Costas Nicou <nicoucostas@gmail.com>
2025-11-04 00:14:18 +0200

    10_products_per_category

--- leviotech/app/views.py
+++ leviotech/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from . models import ProductCategory, Product
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 def homepage(request):
     # test='hello world'
@@ -10,9 +10,12 @@ def homepage(request):
     first_four_best_deals = best_deals[:4]
     last_four_best_deals = best_deals.order_by('-id')[:4]  # last 4 by id descending
     best_deal_star = Product.objects.filter(best_deal_star=True).first()
-    category_laptops = Product.objects.filter(category__name="Laptops")[:5]
-    category_pcs = Product.objects.filter(category__name="PC'S")[:5]
-    category_keyboards = Product.objects.filter(category__name="Keyboards")[:5]
+    category_laptops = Product.objects.filter(category__name="Laptops")[:10]
+    category_pcs = Product.objects.filter(category__name="PC'S")[:10]
+    category_keyboards = Product.objects.filter(category__name="Keyboards")[:10]
+    category_mouses = Product.objects.filter(category__name="Mouse")[:10]
+    category_headsets = Product.objects.filter(category__name="Headset")[:10]
+    category_chairs = Product.objects.filter(category__name="Gaming Chairs")[:10]
     return render(request,'app/homepage.html',{
         'menu_categories':menu_categories,
         'featured_products':featured_products,
@@ -22,6 +25,9 @@ def homepage(request):
         'category_laptops':category_laptops,
         'category_pcs':category_pcs,
         'category_keyboards':category_keyboards,
+        'category_mouses':category_mouses,
+        'category_headsets':category_headsets,
+        'category_chairs':category_chairs,
 
     })
 
@@ -47,10 +53,24 @@ def category_detail(request, slug):
     category = get_object_or_404(ProductCategory, slug=slug)
     menu_categories = ProductCategory.objects.all()
     related_products = category.products.all()
+    # post_list = Post.objects.filter(published=True).order_by('-created_at')
+
+    paginator = Paginator(related_products, 10)  # 5 posts per page
+    page_number = request.GET.get('page')
+
+    try:
+        products = paginator.page(page_number)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+
 
     return render(request, 'app/category.html', {
         'menu_categories':menu_categories,
         'category':category,
         'related_products':related_products,
+        'products':products
     })
 

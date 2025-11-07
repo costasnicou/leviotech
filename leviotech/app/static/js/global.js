fb4173441aa087011281a12a3bb7874d5956fb27
Costas Nicou <nicoucostas@gmail.com>
2025-11-08 00:27:15 +0200

    partial-homepage-rwd

--- leviotech/app/static/js/global.js
+++ leviotech/app/static/js/global.js
@@ -1,6 +1,25 @@
 const amzMsg = document.querySelector('.amz-msg');
 const closeMsgBtn = document.querySelector('.close-msg-btn');
-
+const slider = document.querySelector('.slider');
+const mq = window.matchMedia("(min-width: 320px) and (max-width: 525px)");
 closeMsgBtn.addEventListener('click',function(){
     amzMsg.style.display = 'none';
+    applyMargin(mq);
+    // slider.style.marginTop = "173px";
+
+
 });
+
+
+function applyMargin(e) {
+  if (e.matches) {
+    // Media query is active
+    slider.style.marginTop = "173px";
+  } else {
+    // Reset if outside media query (optional)
+    slider.style.marginTop = "141px";
+  }
+}
+
+// Run on window resize
+mq.addEventListener("change", applyMargin);
